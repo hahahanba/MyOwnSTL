eb0a3724211163bacc4818e10f9643f021284073
Your Name <you@example.com>
2023-01-29 23:22:04 +0800

    update rb_tree.h

---
+++ algorithm.h
@@ -0,0 +1,21 @@
+//
+// Created by gy gao on 1/14/23.
+//
+
+#ifndef MYTINYSTL_ALGORITHM_H
+#define MYTINYSTL_ALGORITHM_H
+
+// 这个头文件包含了 mystl 的所有算法，包括基本算法，数值算法，heap 算法，set 算法和其他算法
+
+#include "algobase.h"
+#include "algo.h"
+#include "set_algo.h"
+#include "heap_algo.h"
+#include "numeric.h"
+
+namespace mystl
+{
+
+}
+
+#endif //MYTINYSTL_ALGORITHM_H
